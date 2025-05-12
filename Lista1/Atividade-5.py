046d871723149e13e28c207670f023dd94ba19a3
EluaneSantos <eluane_santos@estudante.sesisenai.org.br>
2025-05-12 14:20:25 +0000

    update

---
+++ Lista1/Atividade-5.py
@@ -0,0 +1,6 @@
+# Entrada das informações  
+blusas = int(input("Quantidade de blusas: "))  #Pergunta ao usuário quantas blusas ele deseja produzir  
+# Cálculo da quantidade total de metros necessários 
+novelos = (blusas * 120) / 125
+# Saída do resultado  
+print(f"Novelos necessários: {novelos}")
---
+++ Lista2/Atividade-1.py
@@ -0,0 +1,10 @@
+# Lê as dimensões do terreno
+print("_"*60)
+comprimento = float(input("Digite o comprimento do terreno: "))
+largura = float(input("Digite a largura do terreno: "))
+
+# Calcula a área
+area = comprimento * largura
+print("_"*60)
+# Exibe a área
+print("A área do terreno é", area, "unidades quadradas.")
