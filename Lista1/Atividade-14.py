5f2cf976205bb8e468cb480bee3f1e1118c96741
EluaneSantos <eluane_santos@estudante.sesisenai.org.br>
2025-05-14 12:06:26 +0000

    update

--- Lista1/Atividade-14.py
+++ Lista1/Atividade-14.py
@@ -1,3 +1,9 @@
-preco = float(input("Digite o preço do produto: "))
-novo_preco = preco * 0.9
-print("Novo preço:", novo_preco)
+# Dados
+preco = float(input("Informe o preço do produto: "))
+
+# Cálculo do novo preço com desconto
+desconto = preco * 0.10
+novo_preco = preco - desconto
+
+# Resultado
+print("O valor  a ser pago ao valor a pagar:", novo_preco)
